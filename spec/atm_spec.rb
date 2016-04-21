154e372fce829266428894bbf06ec189405f96b7
callea2 <callea2@gmail.com>
2016-04-21 13:17:56 +0000

    Added reject method

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -20,4 +20,9 @@ describe Atm do
     subject.withdraw(50, account)
     expect(subject.funds).to eq 950
   end
+  
+  it 'reject withdraw if account has insufficient funds' do
+    expected_output = { status: true, message: 'insufficient funds', date: Date.today }
+    expect(subject.withdraw(105, account)).to eq expected_output
+  end
 end
